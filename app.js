8a242e2824077421eda56ca22967dac841942f4b
Mustafa Ali <mustafaaaly27@gmail.com>
2024-04-17 22:04:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// var num =100.30123123;
+// console.log(typeof num)
+// // console.log(num.toFixed(3));
+// var num1 = parseFloat(num)
+// var num2 = parseInt(num)
+// console.log(typeof num1 + num1.toFixed(3));
+// console.log(typeof num2 + num2);
+
+
+
+
+// var now = new Date().toString() 
+// console.log(typeof now)
+// console.log(now);
+// console.log(now.slice(0,3));
+
+
+// var now = new Date("17 april 2024")
+// console.log(now);
+
+
+
+// var now1 = new Date().toDateString(); // shows day and date 
+// var now2 = new Date().toLocaleDateString(); // show only date
+// var now3 = new Date().toTimeString();// time with global standard
+// var now4 = new Date().toLocaleTimeString();// shows time only 
+// var now5 = new Date().toLocaleString(); //shows both time and date with am pm
+
+
+// console.log(now1);
+// console.log(now2);
+// console.log(now3);
+// console.log(now4);
+// console.log(now5);
+
+
+
+
+
+
